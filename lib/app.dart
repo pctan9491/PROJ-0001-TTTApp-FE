dbb9f31416b81097bfb0928a0afbfded7d13978d
Tan PC <pctan9491@gmail.com>
2025-12-01 22:07:04 +0800

    Implemented task UI

--- lib/app.dart
+++ lib/app.dart
@@ -12,8 +12,12 @@ class ToDoApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 66, 132, 100)),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ToDo Target App'),
+          centerTitle: false,
+        ),
+        body: const Center(
           child: Text('Architecture Ready! Implement Features in lib/features'),
         ),
       ),
